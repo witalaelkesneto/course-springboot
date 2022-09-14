10b36e36eb9ac342a5cf98ce5ea96bc87a753f3e
witalaelkesneto <100223334+witalaelkesneto@users.noreply.github.com>
2022-09-14 14:45:42 -0300

    Category

---
+++ src/main/java/com/eteg/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.eteg.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eteg.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
