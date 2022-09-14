af319567489182cad8a835f35b247b47708afa21
witalaelkesneto <100223334+witalaelkesneto@users.noreply.github.com>
2022-09-14 15:32:35 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/eteg/course/entities/Product.java
+++ src/main/java/com/eteg/course/entities/Product.java
@@ -9,8 +9,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 @Entity
 @Table(name = "tb_product")
@@ -21,21 +23,22 @@ public class Product implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	private String drecription;
+	private String description;
 	private Double price;
 	private String imgUrl;
 
-	@Transient
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 
 	public Product() {
 	}
 
-	public Product(Long id, String name, String drecription, Double price, String imgUrl) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.drecription = drecription;
+		this.description = description;
 		this.price = price;
 		this.imgUrl = imgUrl;
 	}
@@ -56,12 +59,12 @@ public class Product implements Serializable {
 		this.name = name;
 	}
 
-	public String getDrecription() {
-		return drecription;
+	public String getDescription() {
+		return description;
 	}
 
-	public void setDrecription(String drecription) {
-		this.drecription = drecription;
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public Double getPrice() {
